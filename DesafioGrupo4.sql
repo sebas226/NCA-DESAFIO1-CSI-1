174ea3bd4e7ae034ca33d4af4a277ebc71baedd7
Daniel PROF <danirega2007@gmail.com>
2026-04-09 09:23:43 +0200

    Segunda modificacion de la base de datos

--- DesafioGrupo4.sql
+++ DesafioGrupo4.sql
@@ -1,10 +1,19 @@
 create database Desafio_Grupo4;
 use Desafio_Grupo4;
 
+create table Usuario(
+id_usuario int auto_increment primary key,
+nombre_usuario varchar(50) not null,
+email varchar(150) 
+	check (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') not null,
+contraseña varchar(50) not null
+);
+
 CREATE TABLE Autor (
     id_autor INT AUTO_INCREMENT PRIMARY KEY,
     nombre_autor VARCHAR(50) NOT NULL
 );
+
 CREATE TABLE Libro (
     id_libro INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
@@ -14,10 +23,11 @@ CREATE TABLE Libro (
     id_autor INT,
     FOREIGN KEY (id_autor)
         REFERENCES Autor (id_autor)
-        ON DELETE CASCADE ON UPDATE CASCADE
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 CREATE TABLE Prestamo (
-    id_prestamo INT AUTO_INCREMENT PRIMARY KEY,
+    id_prestamo INT PRIMARY KEY,
     fecha_prestamo DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     fecha_devolucion DATETIME,
     id_usuario INT,
@@ -27,25 +37,36 @@ CREATE TABLE Prestamo (
     FOREIGN KEY (id_libro)
         REFERENCES Libro (id_libro)
 );
-create table Usuario(
-id_usuario int auto_increment primary key,
-nombre_usuario varchar(50) not null,
-email varchar(150) 
-	check (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') not null,
-contraseña varchar(50) not null
-
-);
-CREATE TABLE empleado (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    salario DECIMAL(6 , 2 ) NOT NULL,
+CREATE TABLE Empleado (
+    id_usuario INT PRIMARY KEY,
+    salario DECIMAL(6, 2) NOT NULL,
     tiempo_trabajado DATETIME NOT NULL,
     FOREIGN KEY (id_usuario)
         REFERENCES usuario (id_usuario)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE TABLE cliente (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Cliente (
+    id_usuario INT PRIMARY KEY,
     FOREIGN KEY (id_usuario)
         REFERENCES usuario (id_usuario)
         ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE libro_fisico (
+    id_libro INT PRIMARY KEY,
+    ubicacion VARCHAR(100),
+    estado VARCHAR(50),
+    FOREIGN KEY (id_libro)
+        REFERENCES Libro(id_libro)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+CREATE TABLE libro_digital (
+    id_libro INT PRIMARY KEY,
+    formato VARCHAR(20),
+    tamaño_archivo INT,
+    enlace_descarga VARCHAR(255),
+    FOREIGN KEY (id_libro)
+        REFERENCES Libro(id_libro)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
