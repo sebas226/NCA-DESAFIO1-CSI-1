c90adc304d66f7b000d6c3df7fec6d97eb2475aa
Daniel PROF <danirega2007@gmail.com>
2026-04-17 11:30:50 +0200

    Base de Datos actualizacion

--- BASE_DE_DATOS/DesafioGrupo4.sql
+++ BASE_DE_DATOS/DesafioGrupo4.sql
@@ -1,16 +1,26 @@
 create database Desafio_Grupo4;
+-- drop database desafio_grupo4;
 use Desafio_Grupo4;
 
 create table Usuario(
 id_usuario int auto_increment primary key,
 nombre_usuario varchar(50) not null,
 email varchar(150),
-contraseña varchar(50) not null
+contraseña varchar(50) not null,
+telefono varchar(9) not null,
+direccion varchar(100) not null
 );
 
 CREATE TABLE Autor (
     id_autor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_autor VARCHAR(50) NOT NULL
+    nombre_autor VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE,
+    fecha_fallecimiento DATE,
+    estilo_literario ENUM('Realismo mágico','Ciencia ficción','Fantasía',
+		'Romance','Terror','Policial','Histórico','Poesía','Ensayo') ,
+    numero_libros INT DEFAULT 0,
+    activo BOOLEAN DEFAULT TRUE
+    
 );
 
 CREATE TABLE Libro (
@@ -26,7 +36,7 @@ CREATE TABLE Libro (
         ON UPDATE CASCADE
 );
 CREATE TABLE Prestamo (
-    id_prestamo INT PRIMARY KEY,
+    id_prestamo INT AUTO_INCREMENT PRIMARY KEY,
     fecha_prestamo DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     fecha_devolucion DATETIME,
     id_usuario INT,
@@ -46,6 +56,10 @@ CREATE TABLE Empleado (
 );
 CREATE TABLE Cliente (
     id_usuario INT PRIMARY KEY,
+    numero_socio VARCHAR(20) UNIQUE,
+    fecha_registro DATE,
+    limite_prestamos INT DEFAULT 3,
+    multas DECIMAL(6,2) DEFAULT 0,
     FOREIGN KEY (id_usuario)
         REFERENCES usuario (id_usuario)
         ON DELETE CASCADE ON UPDATE CASCADE
@@ -53,7 +67,7 @@ CREATE TABLE Cliente (
 CREATE TABLE libro_fisico (
     id_libro INT PRIMARY KEY,
     ubicacion VARCHAR(100),
-    estado VARCHAR(50),
+    estado ENUM('Disponible','No Disponible'),
     FOREIGN KEY (id_libro)
         REFERENCES Libro(id_libro)
         ON DELETE CASCADE
@@ -69,3 +83,53 @@ CREATE TABLE libro_digital (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+
+-- INSERT INTO ---------------------------------------------------------------
+
+
+use Desafio_Grupo4;
+
+-- USUARIOS
+INSERT INTO Usuario (nombre_usuario, email, contraseña, telefono, direccion)
+VALUES 
+('juan123', 'juan@email.com', '1234','666111222', ' calle Giovanni' ),
+('maria456', 'maria@email.com', 'abcd','333222111', 'calle Santo Angel');
+
+-- AUTORES
+INSERT INTO Autor (nombre_autor)
+VALUES 
+('Gabriel García Márquez'),
+('J.K. Rowling');
+
+-- LIBROS
+INSERT INTO Libro (titulo, genero, cantidad_total, cantidad_disponible)
+VALUES 
+('Cien años de soledad', 'Novela', 10, 10),
+('Harry Potter', 'Fantasía', 15, 15);
+
+-- CLIENTES
+
+ 
+-- EMPLEADO
+INSERT INTO Empleado (id_usuario, salario, tiempo_trabajado)
+VALUES 
+(1,1200.50, '2024-01-01 08:00:00');
+
+-- LIBRO FÍSICO
+INSERT INTO libro_fisico (id_libro,ubicacion, estado)
+VALUES 
+(1, 'Estante A1', 'Disponible'),
+(2, ' Estante B2', 'Disponible');
+
+-- LIBRO DIGITAL
+INSERT INTO libro_digital (id_libro, formato, tamaño_archivo, enlace_descarga)
+VALUES 
+(1, 'PDF', 5000, 'http://descargas.com/libro1.pdf'),
+(2, 'EPUB', 3000, 'http://descargas.com/libro2.epub');
+
+-- PRÉSTAMOS
+INSERT INTO Prestamo (fecha_devolucion)
+VALUES 
+('2026-04-20 10:00:00'),
+('2026-04-25 12:00:00');
